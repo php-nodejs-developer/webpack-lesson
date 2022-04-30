f7666f7128f59d424439461da94c541bec328cf0
aud203 <aud203@gmail.com>
2022-04-30 12:37:58 +0300

    ++

---
+++ src/js/js-import.js
@@ -0,0 +1,11 @@
+// ./ - соседний файл или директория
+import {sum, max} from "./libs/js-module.js";
+// импорт классов, объектов, функций,
+// которые экспортированы с default:
+// import имя from "./libs/some-file.js";
+
+let z = 56;
+let q = 100;
+
+// подключение в html
+// <script src="js/js-import.js" type="module"></script>
